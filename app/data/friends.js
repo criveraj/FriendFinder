f5e0c1d2f020981b45078e1b2251f62e287bf763
Claudia Rivera <clariveraj@gamil.com>
2017-09-17 17:54:41 -0700

    friends hwk

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+    "name": "Maria",
+    "photo": "http://pngimg.com/uploads/face/face_PNG5646.png",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Justin Bieber",
+    "photo": "https://www.auduno.com/clmtrackr/examples/media/Justin-Bieber2_crop.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+        "name": "Keeyiok",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Flickr_-_nicogenin_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29_-_Jared_Leto_%2814%29.jpg/170px-Flickr_-_nicogenin_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29_-_Jared_Leto_%2814%29.jpg",
+        "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+        ]
+        }
+]
+
+module.exports = friends;
